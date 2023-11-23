3215663adea9df5bf80badb61576beb749b8a0c1
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2023-11-23 19:58:34 +0500

    Update task_4.h

--- src/task_4.h
+++ src/task_4.h
@@ -1,1 +1,26 @@
+#include <iostream>
+#include <cmath>
 
+using namespace std;
+
+void prime_check(int a, int div){
+    if(a==2){
+        cout<<"YES";
+        return;
+    } else if(a%div==0){
+        cout<<"NO";
+        return;
+    } else if(div> ::sqrt(a)){
+        cout<<"YES";
+        return;
+    }
+
+    prime_check(a,div+1);
+}
+int main(){
+    int n=2;
+    int a;
+    cin>>a;
+    prime_check(a,n);
+}
+//time complexity O(n) (worst case)
