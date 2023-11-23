1b52a5963c335c16d5f808393f213edc51ec903d
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2023-11-23 18:45:33 +0500

    Update task_2.h

--- src/task_2.h
+++ src/task_2.h
@@ -1,1 +1,22 @@
+#include <iostream>
 
+using namespace std;
+
+bool pow_of_two(int n) {
+    if (n == 0)
+        return 0;
+    while (n != 1) {
+        if (n % 2 != 0)
+            return 0;
+        n = n / 2;
+    }
+    return 1;
+}
+
+// Driver code
+int main() {
+    int a;
+    cin >> a;
+    pow_of_two(a) ? cout << "YES\n" : cout << "NO\n";
+    return 0;
+}
