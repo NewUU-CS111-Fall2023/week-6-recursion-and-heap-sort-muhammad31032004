c72ac044b4ed4de918db2005725114656d465a9f
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2023-11-23 19:49:20 +0500

    Update task_3.h

--- src/task_3.h
+++ src/task_3.h
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std;
+
+int sum_of_digits (int a) {
+    if (a == 0) {
+        return 0;
+    }
+    return (a % 10 + sum_of_digits(a / 10));
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << sum_of_digits(n);
+}
+//time complexity O(loga) or O(n)
+//a->input number, n->number of digits
