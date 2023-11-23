a9111c1cb20221de3f209613322c9f692bfecb20
muhammad31032004 <127374110+muhammad31032004@users.noreply.github.com>
2023-11-23 19:01:47 +0500

    Update task_1.h

--- src/task_1.h
+++ src/task_1.h
@@ -2,18 +2,31 @@
 
 using namespace std;
 
+void print_out(int a, int b) {
+    cout << a << " ";
+    if (a==b) {
+        return;
+    }
+    print_out(a+1, b);
+}
+
+void print_out_ro(int a, int b) {
+    cout << a << " ";
+    if (a==b) {
+        return;
+    }
+    print_out_ro(a-1, b);
+}
+
 int main() {
     int A, B;
     cin >> A;
     cin >> B;
     if (A>B) {
-        for (int i=A; i>=B; i--) {
-            cout << i << " ";
-        }
+        print_out_ro(A, B);
     } else {
-        for (int i=A; i<=B; i++) {
-            cout << i << " ";
-        }
+        print_out(A, B);
     }
     return 0;
 }
+//time complexity O(n)
